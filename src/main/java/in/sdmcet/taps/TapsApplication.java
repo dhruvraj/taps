a2ce93ca1ae06f76dce0b5350758935446deff5a
DHRUV RAJ SINGH <dhruvraj.singh313@gmail.com>
2016-12-29 12:48:37 +0530

    New Repo Taps

---
+++ src/main/java/in/sdmcet/taps/TapsApplication.java
@@ -0,0 +1,12 @@
+package in.sdmcet.taps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapsApplication.class, args);
+	}
+}
